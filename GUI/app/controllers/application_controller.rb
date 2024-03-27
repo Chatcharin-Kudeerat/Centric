f5de3fb31fcc96f9dae98929ca0b8d891e6471c4
Chatcharin-Kudeerat <44354160+Chatcharin-Kudeerat@users.noreply.github.com>
2024-03-27 12:54:00 +0700

    alert message

--- GUI/app/controllers/application_controller.rb
+++ GUI/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
     helper_method :is_logged_in?
     include ApplicationHelper
+    
     def is_logged_in?
         return redirect_to login_path() if session["current_user"].blank?
     end
