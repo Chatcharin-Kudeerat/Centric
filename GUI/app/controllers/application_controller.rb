40b66a984c6580e0a877f648dbd0b0cc85e36fd9
Chatcharin-Kudeerat <44354160+Chatcharin-Kudeerat@users.noreply.github.com>
2024-03-27 14:16:57 +0700

    edit redirect root

--- GUI/app/controllers/application_controller.rb
+++ GUI/app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
     helper_method :is_logged_in?
     include ApplicationHelper
-    
+
     def is_logged_in?
-        return redirect_to login_path() if session["current_user"].blank?
+        if session["current_user"].blank?
+            redirect_to login_path()
+            return
+        end 
     end
 end
