03e7db933d4c931daced4d942f207a82433050ab
Chatcharin-Kudeerat <44354160+Chatcharin-Kudeerat@users.noreply.github.com>
2024-03-27 17:18:18 +0700

    add captcha

--- GUI/app/controllers/login_controller.rb
+++ GUI/app/controllers/login_controller.rb
@@ -7,23 +7,29 @@ class LoginController < ApplicationController
         organization = params[:form_login][:organization]
         username = params[:form_login][:username]
         password = params[:form_login][:password]
-
-        if get_authen[organization].present?
-            destination = login_path(organization: organization, username: username)
-            get_authen[organization].each do |h|
-                if h["username"] == username && h["password"] == password
-                    session["current_user"] = h
-                    session["current_organization"] = organization
-                    destination = root_path()
-                    flash[:success] = "Login succussfully! Welcome #{h["firstname"]} to ESAS GUI"
-                    break
+        captcha = params[:form_login][:captcha]
+        
+        if captcha == "W68HP"
+            if get_authen[organization].present?
+                destination = login_path(organization: organization, username: username)
+                get_authen[organization].each do |h|
+                    if h["username"] == username && h["password"] == password
+                        session["current_user"] = h
+                        session["current_organization"] = organization
+                        destination = root_path()
+                        flash[:success] = "Login succussfully! Welcome #{h["firstname"]} to ESAS GUI"
+                        break
+                    end
                 end
-            end
-            flash[:error] = "Login failed! username or password incorrect. Please try again later" if session["current_user"].blank?
-            redirect_to destination
+                flash[:error] = "Login failed! username or password incorrect. Please try again later" if session["current_user"].blank?
+                redirect_to destination
 
+            else
+                flash[:error] = "Login failed! oranization ID not found. Please try again later"
+                redirect_to login_path(organization: organization, username: username)
+            end
         else
-            flash[:error] = "Login failed! oranization ID not found. Please try again later"
+            flash[:error] = "Captcha incorrect. Please try again later"
             redirect_to login_path(organization: organization, username: username)
         end
     end
