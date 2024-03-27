054274cbe49c506b5e4b13b6eb0d967f44eba422
Chatcharin-Kudeerat <44354160+Chatcharin-Kudeerat@users.noreply.github.com>
2024-03-27 16:05:16 +0700

    add date label

--- GUI/app/controllers/application_controller.rb
+++ GUI/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
     helper_method :is_logged_in?
     include ApplicationHelper
+    require "date"
 
     def is_logged_in?
         if session["current_user"].blank?
